bcab112172eb633584fae3c45ee35b4f161ad9ce
Yariyari <yaritza.urbina.sanhueza@gmail.com>
2022-10-21 11:07:59 -0300

    Sql ejercicios

---
+++ migrations/05-seed_data.sql
@@ -0,0 +1,4 @@
+\c ecommerce
+
+\COPY customers FROM 'C:\Users\user\Intro_Dev\sql-test-prep-Y-X-X-S\data\customers.csv' csv HEADER
+\COPY products FROM 'C:\Users\user\Intro_Dev\sql-test-prep-Y-X-X-S\data\products.csv' csv HEADER
